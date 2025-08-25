ef592f4ba2589bcecf97ed175c09f72ec72400b2
Amelia Druzba <ameliadruzba2004@gmail.com>
2025-08-25 13:40:59 +0200

    zmiana na float zamiast int

--- kalkulator.py
+++ kalkulator.py
@@ -1,18 +1,18 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename='loggi.log')
 
-def dodawanie(*args:int):
+def dodawanie(*args:float):
     logging.debug(f'wartosci podane dla dzialania DODAWANIE to {(list(args))}\nroznica wynosi = {sum(args)}')
     return sum(args)
 
-def odejmowanie(*args:int):
+def odejmowanie(*args:float):
     baza = args[0]
     for i in args[1:]:
         baza -= i
     logging.debug(f'wartosci podane dla dzialania ODEJMOWANIE to {(list(args))}\nroznica wynosi = {baza}')
     return baza
 
-def mnozenie(*args:int):
+def mnozenie(*args:float):
     liczba = 1
     for i in args:
         liczba *= i
@@ -20,7 +20,7 @@ def mnozenie(*args:int):
     return liczba
 
 
-def dzielenie(*args:int):
+def dzielenie(*args:float):
     start = args[0]
     for i in args[1:]:
         start /= i
@@ -30,20 +30,20 @@ def dzielenie(*args:int):
 while True:
     zapytanie_o_dzialanie = input(f'Podaj jakie dzialanie chcesz wykonac:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie\n5.koniec programu\n')
     if zapytanie_o_dzialanie == "1":
-        liczby = (input('Podaj liczby po przecinku jakie chcesz do siebie dodac: ')).split(',')
-        liczby = [int(x.strip()) for x in liczby]
+        liczby = (input('Podaj liczby po spacji jakie chcesz do siebie dodac: ')).split(' ')
+        liczby = [float(x.strip()) for x in liczby]
         print(dodawanie(*liczby))
     if zapytanie_o_dzialanie == "2":
-        ode = (input('Podaj liczby po przecinku jakie chcesz do siebie odjac: ')).split(',')
-        ode = [int(x) for x in ode]
+        ode = (input('Podaj liczby po spacji jakie chcesz do siebie odjac: ')).split(' ')
+        ode = [float(x) for x in ode]
         print(odejmowanie(*ode))
     if zapytanie_o_dzialanie == "3":
-        mno = (input('Podaj liczby po przecinku jakie chcesz pomnozyc: ')).split(',')
-        mno = [int(x) for x in mno]
+        mno = (input('Podaj liczby po spacji jakie chcesz pomnozyc: ')).split(' ')
+        mno = [float(x) for x in mno]
         print(mnozenie(*mno))
     if zapytanie_o_dzialanie == "4":
-        dzie = (input('Podaj liczby po przecinku jakie chcesz podzielic: ')).split(',')
-        dzie = [int(x) for x in dzie]
+        dzie = (input('Podaj liczby po spacji jakie chcesz podzielic: ')).split(' ')
+        dzie = [float(x) for x in dzie]
         print(dzielenie(*dzie))
     if zapytanie_o_dzialanie == "5":
         break
