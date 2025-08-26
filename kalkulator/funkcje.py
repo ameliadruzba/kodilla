56ed808eb48f8423bc6014794f422895cf2597d9
Amelia Druzba <ameliadruzba2004@gmail.com>
2025-08-26 12:01:04 +0200

    zmiana zmiannych

--- kalkulator/funkcje.py
+++ kalkulator/funkcje.py
@@ -1,18 +1,18 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename='loggi.log')
 
-def dodawanie(*args:float):
+def add(*args:float):
     logging.debug(f'wartosci podane dla dzialania DODAWANIE to {(list(args))}\nroznica wynosi = {sum(args)}')
     return sum(args)
 
-def odejmowanie(*args:float):
+def sub(*args:float):
     baza = args[0]
     for i in args[1:]:
         baza -= i
     logging.debug(f'wartosci podane dla dzialania ODEJMOWANIE to {(list(args))}\nroznica wynosi = {baza}')
     return baza
 
-def mnozenie(*args:float):
+def multi(*args:float):
     liczba = 1
     for i in args:
         liczba *= i
@@ -20,7 +20,7 @@ def mnozenie(*args:float):
     return liczba
 
 
-def dzielenie(*args:float):
+def div(*args:float):
     start = args[0]
     for i in args[1:]:
         start /= i
--- kalkulator/kalkulator.py
+++ kalkulator/kalkulator.py
@@ -1,22 +1,22 @@
-from funkcje import dodawanie, odejmowanie, mnozenie, dzielenie
+from funkcje import add, sub, multi, div
 
 while True:
-    zapytanie_o_dzialanie = input(f'Podaj jakie dzialanie chcesz wykonac:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie\n5.koniec programu\n')
-    if zapytanie_o_dzialanie == "1":
-        liczby = (input('Podaj liczby po spacji jakie chcesz do siebie dodac: ')).split(' ')
-        liczby = [float(x.strip()) for x in liczby]
-        print(dodawanie(*liczby))
-    if zapytanie_o_dzialanie == "2":
-        ode = (input('Podaj liczby po spacji jakie chcesz do siebie odjac: ')).split(' ')
-        ode = [float(x) for x in ode]
-        print(odejmowanie(*ode))
-    if zapytanie_o_dzialanie == "3":
-        mno = (input('Podaj liczby po spacji jakie chcesz pomnozyc: ')).split(' ')
-        mno = [float(x) for x in mno]
-        print(mnozenie(*mno))
-    if zapytanie_o_dzialanie == "4":
-        dzie = (input('Podaj liczby po spacji jakie chcesz podzielic: ')).split(' ')
-        dzie = [float(x) for x in dzie]
-        print(dzielenie(*dzie))
-    if zapytanie_o_dzialanie == "5":
+    operation = input(f'Podaj jakie dzialanie chcesz wykonac:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie\n5.koniec programu\n')
+    if operation == "1":
+        number = (input('Podaj liczby po spacji jakie chcesz do siebie dodac: ')).split(' ')
+        number = [float(x.strip()) for x in number]
+        print(add(*number))
+    if operation == "2":
+        subtraction = (input('Podaj liczby po spacji jakie chcesz do siebie odjac: ')).split(' ')
+        subtraction = [float(x) for x in subtraction]
+        print(sub(*subtraction))
+    if operation == "3":
+        multiply = (input('Podaj liczby po spacji jakie chcesz pomnozyc: ')).split(' ')
+        multiply = [float(x) for x in multiply]
+        print(multi(*multiply))
+    if operation == "4":
+        division = (input('Podaj liczby po spacji jakie chcesz podzielic: ')).split(' ')
+        division = [float(x) for x in division]
+        print(div(*division))
+    if operation == "5":
         break
